bde645da3e67a675c1967cf198883b32f06ad368
Omego2K <omego2k@gmail.com>
2020-01-09 21:28:26 -0500

    add option methods and AssertRequality extension

---
+++ core/FluentAssertions.Optional/Extensions/ObjectExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using FluentAssertions.Equivalency;
+
+namespace FluentAssertions.Optional.Extensions
+{
+    public static class ObjectExtensions
+    {
+        public static void AssertEquality<TExpected, TValue>(this object? value, TExpected expected,
+            Func<EquivalencyAssertionOptions<TValue>, EquivalencyAssertionOptions<TValue>> config, string? because = null,
+            params object[] becauseArgs)
+        {
+            var assertionOptions = config(AssertionOptions.CloneDefaults<TValue>()).AsCollection();
+            var context = new EquivalencyValidationContext
+            {
+                Subject = value,
+                Expectation = expected,
+                RootIsCollection = true,
+                CompileTimeType = typeof(TExpected),
+                Because = because,
+                BecauseArgs = becauseArgs,
+                Tracer = assertionOptions.TraceWriter
+            };
+            
+            new EquivalencyValidator(assertionOptions).AssertEquality(context);
+        }
+    }
+}
--- tests/unit/FluentAssertions.Optional.Tests.Unit/OptionAssertionsMaybeTests.cs
+++ tests/unit/FluentAssertions.Optional.Tests.Unit/OptionAssertionsMaybeTests.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Drawing;
+using JetBrains.Annotations;
 using Optional;
 using Xunit;
 
@@ -36,6 +38,71 @@ namespace FluentAssertions.Optional.Tests.Unit
             Action haveSome = () => assertions.HaveSome();
             haveSome.Should().Throw<Exception>().WithMessage("Option does not have a value.");
         }
+
+        [Fact]
+        public void HasValueEquivalentToThrowsExceptionWhenOptionHasNone()
+        {
+            var assertions = new OptionAssertions<string>(Option.None<string>());
+            Action hasValueEquivalentTo = () => assertions.HasValueEquivalentTo(string.Empty);
+            hasValueEquivalentTo.Should().Throw<Exception>().WithMessage("Option does not have a value.");
+        }
+
+        [Fact]
+        public void HasValueEquivalentToWithOptionsThrowsExceptionWhenOptionHasNone()
+        {
+            var assertions = new OptionAssertions<string>(Option.None<string>());
+            Action hasValueEquivalentTo = () => assertions.HasValueEquivalentTo(string.Empty, options => options);
+            hasValueEquivalentTo.Should().Throw<Exception>().WithMessage("Option does not have a value.");
+        }
+
+        [Fact]
+        public void HasValueEquivalentToThrowsExceptionWhenOptionDoesNotHaveExpectedValue()
+        {
+            var assertions = new OptionAssertions<string>("test".Some());
+            Action hasValueEquivalentTo = () => assertions.HasValueEquivalentTo("Test");
+            hasValueEquivalentTo.Should()
+                                .Throw<Exception>()
+                                .WithMessage("Expected string to be \"Test\", but \"test\" differs near \"tes\" " +
+                                             "(index 0).\r\n\nWith configuration:\n- Use declared types and members\r\n- Compare enums " +
+                                             "by value\r\n- Include all non-private properties\r\n- Include all non-private fields\r\n- " +
+                                             "Match member by name (or throw)\r\n- Without automatic conversion.\r\n- Without automatic conversion.\r\n- Be strict about the order of items in byte arrays\r\n");
+        }
+
+        [Fact]
+        public void HasValueEquivalentToDoesNotThrowWhenOptionHasExpectedValue()
+        {
+            var assertions = new OptionAssertions<string>("test".Some());
+            Action hasValueEquivalentTo = () => assertions.HasValueEquivalentTo("test");
+            hasValueEquivalentTo.Should().NotThrow();
+        }
+
+        [Fact]
+        public void HasValueEquivalentToWithOptionsDoesNotThrowWhenOptionHasExpectedValue()
+        {
+            var assertions = CreateOptionAssertions(new { Val1 = "test", Val2 = "tset" });
+            Action hasValueEquivalentTo = () => assertions.HasValueEquivalentTo(new { Val1 = "test", Val2 = "tset1" },
+                options => options.Excluding(arg => arg.Val2));
+            hasValueEquivalentTo.Should().NotThrow();
+        }
+
+        [Fact]
+        public void HasValueEquivalentToWithOptionsThrowsExceptionWhenOptionDoesNotHaveExpectedValue()
+        {
+            var assertions = new OptionAssertions<int>(0.Some());
+            Action hasValueEquivalentTo = () =>
+                assertions.HasValueEquivalentTo(KnownColor.ActiveBorder, options => options.ComparingEnumsByName());
+            hasValueEquivalentTo.Should().Throw<Exception>().WithMessage("Expected enum to equal " +
+                                                                         "KnownColor.ActiveBorder(1) by name, but found 0.\r\n\nWith " +
+                                                                         "configuration:\n- Use declared types and members\r\n- " +
+                                                                         "Compare enums by name\r\n- Include all non-private properties" +
+                                                                         "\r\n- Include all non-private fields\r\n- Match member by name " +
+                                                                         "(or throw)\r\n- Without automatic conversion.\r\n- Without " +
+                                                                         "automatic conversion.\r\n- Be strict about the order of items in " +
+                                                                         "byte arrays\r\n");
+        }
+
+        [NotNull]
+        private OptionAssertions<T> CreateOptionAssertions<T>(T value) => new OptionAssertions<T>(value.Some());
 
         [Fact]
         public void SubjectReturnsOptionUsedInConstructor()
